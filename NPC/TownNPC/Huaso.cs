8516afc656f9bd81a23e2b84992eb46b53f5b2a0
Crisxpy <benjaminnunezburboa@gmail.com>
2024-03-21 21:35:01 -0300

    Re-added Huaso NPC, still not complete .

--- NPC/TownNPC/Huaso.cs
+++ NPC/TownNPC/Huaso.cs
@@ -1,12 +1,18 @@
 using ChileMOD.Items.Comida;
+using ChileMOD.NPC.Critter;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using Terraria;
 using Terraria.Audio;
+using Terraria.GameContent;
 using Terraria.GameContent.Bestiary;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.GameContent.Personalities;
+using Terraria.GameContent.UI;
+using Terraria.Graphics.Renderers;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -19,21 +25,40 @@ namespace ChileMOD.NPC.TownNPC
 	[AutoloadHead]
 	public class Huaso : ModNPC
 	{
-		public override void SetStaticDefaults()
-		{
-			Main.npcFrameCount[Type] = 25; // The amount of frames the NPC has
-            int examplePersonType = ModContent.NPCType<NPC.TownNPC.Huaso>(); // Get ExamplePerson's type
-            var guideHappiness = NPCHappiness.Get(NPCID.Guide); // Get the key into The Guide's happiness
+		public const string ShopName = "Shop";
+		public int NumberOfTimesTalkedTo = 0;
 
-            guideHappiness.SetNPCAffection(examplePersonType, AffectionLevel.Love); // Make the Guide love ExamplePerson!
+		private static int ShimmerHeadIndex;
+		private static Profiles.StackedNPCProfile NPCProfile;
 
-            NPCID.Sets.ExtraFramesCount[Type] = 9; // Generally for Town NPCs, but this is how the NPC does extra things such as sitting in a chair and talking to other NPCs.
-			NPCID.Sets.AttackFrameCount[Type] = 4;
-			NPCID.Sets.DangerDetectRange[Type] = 800; // The amount of pixels away from the center of the npc that it tries to attack enemies.
-			NPCID.Sets.AttackType[Type] = 0;
+		public override void SetStaticDefaults() {
+			Main.npcFrameCount[Type] = 25; // The total amount of frames the NPC has
+
+			NPCID.Sets.ExtraFramesCount[Type] = 9; // Generally for Town NPCs, but this is how the NPC does extra things such as sitting in a chair and talking to other NPCs. This is the remaining frames after the walking frames.
+			NPCID.Sets.AttackFrameCount[Type] = 4; // The amount of frames in the attacking animation.
+			NPCID.Sets.DangerDetectRange[Type] = 700; // The amount of pixels away from the center of the NPC that it tries to attack enemies.
+			NPCID.Sets.AttackType[Type] = 0; // The type of attack the Town NPC performs. 0 = throwing, 1 = shooting, 2 = magic, 3 = melee
 			NPCID.Sets.AttackTime[Type] = 90; // The amount of time it takes for the NPC's attack animation to be over once it starts.
-			NPCID.Sets.AttackAverageChance[Type] = 50;
+			NPCID.Sets.AttackAverageChance[Type] = 30; // The denominator for the chance for a Town NPC to attack. Lower numbers make the Town NPC appear more aggressive.
 			NPCID.Sets.HatOffsetY[Type] = 4; // For when a party is active, the party hat spawns at a Y offset.
+			NPCID.Sets.ShimmerTownTransform[NPC.type] = false; // This set says that the Town NPC has a Shimmered form. Otherwise, the Town NPC will become transparent when touching Shimmer like other enemies.
+
+			NPCID.Sets.ShimmerTownTransform[Type] = false; // Allows for this NPC to have a different texture after touching the Shimmer liquid.
+
+			// Connects this NPC with a custom emote.
+			// This makes it when the NPC is in the world, other NPCs will "talk about him".
+			// By setting this you don't have to override the PickEmote method for the emote to appear.
+			//NPCID.Sets.FaceEmote[Type] = ModContent.EmoteBubbleType<ExamplePersonEmote>();
+
+			// Influences how the NPC looks in the Bestiary
+			NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers() {
+				Velocity = 1f, // Draws the NPC in the bestiary as if its walking +1 tiles in the x direction
+				Direction = 1 // -1 is left and 1 is right. NPCs are drawn facing the left by default but Huaso will be drawn facing the right
+				// Rotation = MathHelper.ToRadians(180) // You can also change the rotation of an NPC. Rotation is measured in radians
+				// If you want to see an example of manually modifying these when the NPC is drawn, see PreDraw
+			};
+
+			NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
 
 			// Set Example Person's biome and neighbor preferences with the NPCHappiness hook. You can add happiness text and remarks with localization (See an example in ExampleMod/Localization/en-US.lang).
 			// NOTE: The following code uses chaining - a style that works due to the fact that the SetXAffection methods return the same NPCHappiness instance they're called on.
@@ -44,10 +69,16 @@ namespace ChileMOD.NPC.TownNPC
 				.SetNPCAffection(NPCID.Dryad, AffectionLevel.Love) // Loves living near the dryad.
 				.SetNPCAffection(NPCID.Guide, AffectionLevel.Like) // Likes living near the guide.
 				.SetNPCAffection(NPCID.Merchant, AffectionLevel.Dislike) // Dislikes living near the merchant.
-			;
-        }
-        public override void SetDefaults()
-		{
+			; // < Mind the semicolon!
+
+			// This creates a "profile" for Huaso, which allows for different textures during a party and/or while the NPC is shimmered.
+			//NPCProfile = new Profiles.StackedNPCProfile(
+			//	new Profiles.DefaultNPCProfile(Texture, NPCHeadLoader.GetHeadSlot(HeadTexture), Texture + "_Party"),
+			//	new Profiles.DefaultNPCProfile(Texture + "_Shimmer", ShimmerHeadIndex, Texture + "_Shimmer_Party")
+			//);
+		}
+
+		public override void SetDefaults() {
 			NPC.townNPC = true; // Sets NPC to be a Town NPC
 			NPC.friendly = true; // NPC Will not attack player
 			NPC.width = 18;
@@ -58,13 +89,12 @@ namespace ChileMOD.NPC.TownNPC
 			NPC.lifeMax = 250;
 			NPC.HitSound = SoundID.NPCHit1;
 			NPC.DeathSound = SoundID.NPCDeath1;
-            NPC.aiStyle = 7;
-            NPC.knockBackResist = 0.5f;
+			NPC.aiStyle = 7;
+			NPC.knockBackResist = 0.5f;
 			AnimationType = NPCID.Merchant;
 		}
 
-		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
-		{
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) {
 			// We can use AddRange instead of calling Add multiple times in order to add multiple items at once
 			bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
 				// Sets the preferred biomes of this town NPC listed in the bestiary.
@@ -73,28 +103,52 @@ namespace ChileMOD.NPC.TownNPC
 
 				// Sets your NPC's flavor text in the bestiary.
 				new FlavorTextBestiaryInfoElement("Directo de los campos Chilenos, representante de las costumbres Chilenas, todos somos un poco como durante el 18"),
+
+				// You can add multiple elements if you really wanted to
+				// You can also use localization keys (see Localization/en-US.lang)
+				new FlavorTextBestiaryInfoElement("Mods.ChileMOD.Bestiary.Huaso")
 			});
 		}
 
-		// The PreDraw hook is useful for drawing things before our sprite is drawn or running code before the sprite is drawn
-		// Returning false will allow you to manually draw your NPC
+		//public override void HitEffect(NPC.HitInfo hit) {
+		//	int num = NPC.life > 0 ? 1 : 5;
 
-		public override bool CanTownNPCSpawn(int numTownNPCs, int money)
-		{ // Requirements for the town NPC to spawn.
-			for (int k = 0; k < 255; k++)
-			{
+		//	for (int k = 0; k < num; k++) {
+		//		Dust.NewDust(NPC.position, NPC.width, NPC.height, ModContent.DustType<Sparkle>());
+		//	}
+
+		//	// Create gore when the NPC is killed.
+		//	if (Main.netMode != NetmodeID.Server && NPC.life <= 0) {
+		//		// Retrieve the gore types. This NPC has shimmer and party variants for head, arm, and leg gore. (12 total gores)
+		//		string variant = "";
+		//		if (NPC.IsShimmerVariant) variant += "_Shimmer";
+		//		if (NPC.altTexture == 1) variant += "_Party";
+		//		int hatGore = NPC.GetPartyHatGore();
+		//		int headGore = Mod.Find<ModGore>($"{Name}_Gore{variant}_Head").Type;
+		//		int armGore = Mod.Find<ModGore>($"{Name}_Gore{variant}_Arm").Type;
+		//		int legGore = Mod.Find<ModGore>($"{Name}_Gore{variant}_Leg").Type;
+
+		//		// Spawn the gores. The positions of the arms and legs are lowered for a more natural look.
+		//		if (hatGore > 0) {
+		//			Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, hatGore);
+		//		}
+		//		Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, headGore, 1f);
+		//		Gore.NewGore(NPC.GetSource_Death(), NPC.position + new Vector2(0, 20), NPC.velocity, armGore);
+		//		Gore.NewGore(NPC.GetSource_Death(), NPC.position + new Vector2(0, 20), NPC.velocity, armGore);
+		//		Gore.NewGore(NPC.GetSource_Death(), NPC.position + new Vector2(0, 34), NPC.velocity, legGore);
+		//		Gore.NewGore(NPC.GetSource_Death(), NPC.position + new Vector2(0, 34), NPC.velocity, legGore);
+		//	}
+		//}
+
+		public override bool CanTownNPCSpawn(int numTownNPCs) { // Requirements for the town NPC to spawn.
+			for (int k = 0; k < Main.maxPlayers; k++) {
 				Player player = Main.player[k];
-				if (!player.active)
-				{
+				if (!player.active) {
 					continue;
 				}
-				if (DateTime.Now.Month == 9) {
-					return true;
-				}
 
-				// Jugador debe tener almenos un Choripan o Anticucho
-				if (player.inventory.Any(item => item.type == ModContent.ItemType<Anticucho>() || item.type == ModContent.ItemType<Choripan>()))
-				{
+				// Player has to have either an ExampleItem or an ExampleBlock in order for the NPC to spawn
+				if (player.inventory.Any(item => item.type == ModContent.ItemType<Anticucho>() || item.type == ModContent.ItemType<Choripan>())) {
 					return true;
 				}
 			}
@@ -102,10 +156,11 @@ namespace ChileMOD.NPC.TownNPC
 			return false;
 		}
 
+		public override ITownNPCProfile TownNPCProfile() {
+			return NPCProfile;
+		}
 
-
-		public override List<string> SetNPCNameList()
-		{
+		public override List<string> SetNPCNameList() {
 			return new List<string>() {
 				"Pancho",
 				"Juan",
@@ -116,75 +171,104 @@ namespace ChileMOD.NPC.TownNPC
 			};
 		}
 
-		public override string GetChat()
-		{
+		public override string GetChat() {
 			WeightedRandom<string> chat = new WeightedRandom<string>();
-			chat.Add("Tengo que decir..... Viva Chile mierda!");
-			chat.Add("Me falta un corderito al palo....");
-			chat.Add("La mejor epoca del año, es cuando puedo tomar Terremoto de almuerzo once y cena");
-			chat.Add("Quisiera que esta chicha, me durara toda la vida");
-			chat.Add("Este 18, brindemos por los que estan y los que faltan!");
-			return chat; // chat is implicitly cast to a string.
+			// These are things that the NPC has a chance of telling you when you talk to it.
+			chat.Add(Language.GetTextValue("Mods.ExampleMod.Dialogue.Huaso.StandardDialogue1"));
+			chat.Add(Language.GetTextValue("Mods.ExampleMod.Dialogue.Huaso.StandardDialogue2"));
+			chat.Add(Language.GetTextValue("Mods.ExampleMod.Dialogue.Huaso.StandardDialogue3"));
+			chat.Add(Language.GetTextValue("Mods.ExampleMod.Dialogue.Huaso.StandardDialogue4"));
+			chat.Add(Language.GetTextValue("Mods.ExampleMod.Dialogue.Huaso.CommonDialogue"), 5.0);
+			chat.Add(Language.GetTextValue("Mods.ExampleMod.Dialogue.Huaso.RareDialogue"), 0.1);
+
+			NumberOfTimesTalkedTo++;
+			if (NumberOfTimesTalkedTo >= 10) {
+				//This counter is linked to a single instance of the NPC, so if Huaso is killed, the counter will reset.
+				chat.Add(Language.GetTextValue("Mods.ExampleMod.Dialogue.Huaso.TalkALot"));
+			}
+
+			string chosenChat = chat; // chat is implicitly cast to a string. This is where the random choice is made.
+
+			// Here is some additional logic based on the chosen chat line. In this case, we want to display an item in the corner for StandardDialogue4.
+			if (chosenChat == Language.GetTextValue("Mods.ExampleMod.Dialogue.Huaso.StandardDialogue4")) {
+				// Main.npcChatCornerItem shows a single item in the corner, like the Angler Quest chat.
+				Main.npcChatCornerItem = ItemID.HiveBackpack;
+			}
+
+			return chosenChat;
 		}
-		public override void SetChatButtons(ref string button, ref string button2)
-		{
-			button = "Shop"; 
+
+		public override void SetChatButtons(ref string button, ref string button2) { // What the chat buttons are when you open up the chat UI
+			button = "shop";
 		}
-		public override void OnChatButtonClicked(bool firstButton, ref bool shop)
-		{
-			if (firstButton)
-			{
-				shop = true;
+
+		public override void OnChatButtonClicked(bool firstButton, ref string shop) {
+			if (firstButton) {
+				// We want 3 different functionalities for chat buttons, so we use HasItem to change button 1 between a shop and upgrade action.
+				shop = "Pica del Huasito"; // Name of the shop tab we want to open.
 			}
 		}
-		public override void SetupShop(Chest shop, ref int nextSlot)
-        {
-            base.SetupShop(shop, ref nextSlot);
-            shop.item[nextSlot].SetDefaults(ModContent.ItemType<Choripan>());
-			nextSlot++;
-            shop.item[nextSlot].SetDefaults(ModContent.ItemType<Anticucho>());
-            nextSlot++;
-            shop.item[nextSlot].SetDefaults(ModContent.ItemType<Terremoto>());
-            nextSlot++;
-            shop.item[nextSlot].SetDefaults(ModContent.ItemType<Empanada>());
-            nextSlot++;
-            shop.item[nextSlot].SetDefaults(ModContent.ItemType<Piscola>());
-            nextSlot++;
-        }
-		public override void ModifyNPCLoot(NPCLoot npcLoot)
-		{
-			npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<Choripan>()));
+
+		// Not completely finished, but below is what the NPC will sell
+		public override void AddShops() {
+			var npcShop = new NPCShop(Type, ShopName)
+				//.Add<EquipMaterial>()
+				//.Add<BossItem>()
+				.Add<Items.Comida.Anticucho>()
+				.Add<Items.Comida.Piscola>()
+				.Add<Items.Comida.Empanada>()
+				.Add<Items.Comida.Choripan>()
+				.Add<Items.Comida.Terremoto>();
+			npcShop.Register(); // Name of this shop tab
 		}
 
-		// Make this Town NPC teleport to the King and/or Queen statue when triggered.
+		public override void ModifyActiveShop(string shopName, Item[] items) {
+			foreach (Item item in items) {
+				// Skip 'air' items and null items.
+				if (item == null || item.type == ItemID.None) {
+					continue;
+				}
+			}
+		}
+
+		public override void ModifyNPCLoot(NPCLoot npcLoot) {
+			npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<Items.Comida.Choripan>()));
+		}
+
+		// Make this Town NPC teleport to the King and/or Queen statue when triggered. Return toKingStatue for only King Statues. Return !toKingStatue for only Queen Statues. Return true for both.
 		public override bool CanGoToStatue(bool toKingStatue) => true;
 
-		public override void TownNPCAttackStrength(ref int damage, ref float knockback)
-		{
+		// Make something happen when the npc teleports to a statue. Since this method only runs server side, any visual effects like dusts or gores have to be synced across all clients manually.
+
+		public override void TownNPCAttackStrength(ref int damage, ref float knockback) {
 			damage = 20;
 			knockback = 4f;
 		}
 
-		public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown)
-		{
+		public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown) {
 			cooldown = 30;
 			randExtraCooldown = 30;
 		}
 
-		 public override void TownNPCAttackProj(ref int projType, ref int attackDelay) {
-		 	projType = ProjectileID.CannonballFriendly;
-		 	attackDelay = 3;
-		 }
+		public override void TownNPCAttackProj(ref int projType, ref int attackDelay) {
+			projType = ProjectileID.Bananarang;
+			attackDelay = 1;
+		}
 
-		public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset)
-		{
+		public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset) {
 			multiplier = 12f;
 			randomOffset = 2f;
+			// SparklingBall is not affected by gravity, so gravityCorrection is left alone.
 		}
 
-		internal static void ActivateEighteenPartyMode() {
+		public override void LoadData(TagCompound tag) {
+			NumberOfTimesTalkedTo = tag.GetInt("numberOfTimesTalkedTo");
 		}
+
+		public override void SaveData(TagCompound tag) {
+			tag["numberOfTimesTalkedTo"] = NumberOfTimesTalkedTo;
+		}
+
+		// Let the NPC "talk about" minion boss
 	}
-
-
 }
