20ae185ef8c170090290cbc5d02daab2183ef0e6
Crisxpy <benjaminnunezburboa@gmail.com>
2024-03-22 21:52:46 -0300

    Cambie el proyectil del huasito a un cuchillo.

--- NPC/TownNPC/Huaso.cs
+++ NPC/TownNPC/Huaso.cs
@@ -38,9 +38,9 @@ namespace ChileMOD.NPC.TownNPC
 
 			NPCID.Sets.ExtraFramesCount[Type] = 9; // Generally for Town NPCs, but this is how the NPC does extra things such as sitting in a chair and talking to other NPCs. This is the remaining frames after the walking frames.
 			NPCID.Sets.AttackFrameCount[Type] = 4; // The amount of frames in the attacking animation.
-			NPCID.Sets.DangerDetectRange[Type] = 700; // The amount of pixels away from the center of the NPC that it tries to attack enemies.
+			NPCID.Sets.DangerDetectRange[Type] = 500; // The amount of pixels away from the center of the NPC that it tries to attack enemies.
 			NPCID.Sets.AttackType[Type] = 0; // The type of attack the Town NPC performs. 0 = throwing, 1 = shooting, 2 = magic, 3 = melee
-			NPCID.Sets.AttackTime[Type] = 90; // The amount of time it takes for the NPC's attack animation to be over once it starts.
+			NPCID.Sets.AttackTime[Type] = 100; // The amount of time it takes for the NPC's attack animation to be over once it starts.
 			NPCID.Sets.AttackAverageChance[Type] = 30; // The denominator for the chance for a Town NPC to attack. Lower numbers make the Town NPC appear more aggressive.
 			NPCID.Sets.HatOffsetY[Type] = 4; // For when a party is active, the party hat spawns at a Y offset.
 			NPCID.Sets.ShimmerTownTransform[NPC.type] = false; // This set says that the Town NPC has a Shimmered form. Otherwise, the Town NPC will become transparent when touching Shimmer like other enemies.
@@ -85,7 +85,6 @@ namespace ChileMOD.NPC.TownNPC
 			NPC.friendly = true; // NPC Will not attack player
 			NPC.width = 18;
 			NPC.height = 40;
-			NPC.aiStyle = 7;
 			NPC.damage = 10;
 			NPC.defense = 15;
 			NPC.lifeMax = 250;
@@ -223,13 +222,13 @@ namespace ChileMOD.NPC.TownNPC
 		}
 
 		public override void TownNPCAttackProj(ref int projType, ref int attackDelay) {
-			projType = ProjectileID.BloodyMachete;
-			attackDelay = 1;
+			projType = ProjectileID.ThrowingKnife;
+			attackDelay = 2;
 		}
 
 		public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset) {
 			multiplier = 12f;
-			randomOffset = 2f;
+			randomOffset = 1f;
 			// SparklingBall is not affected by gravity, so gravityCorrection is left alone.
 		}
 	}
